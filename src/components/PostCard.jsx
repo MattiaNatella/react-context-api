ad1a8b507912f8355ad88cb4758c5bf11655fac2
MattiaNatella <mattianatella@gmail.com>
2025-01-27 16:28:02 +0100

    [add] shadow to cards

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -3,7 +3,7 @@ const PostCard = ({ post }) => {
 
     return (
         <div className="col-4 p-2">
-            <div className="card">
+            <div className="card shadow">
                 <img src={post?.image} className="card-img-top" alt={post?.title} />
                 <div className="card-body">
                     <h5 className="card-title">{post?.title}</h5>
